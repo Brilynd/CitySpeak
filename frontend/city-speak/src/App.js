b70e9e99b9ff1afea7d472dfc301273922a92e59
g1776 <gregoryg323@gmail.com>
2022-04-09 21:50:33 -0400

    started search

--- frontend/city-speak/src/App.js
+++ frontend/city-speak/src/App.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+
 import "./App.css"
 import MyMap from './Map';
+
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
 
 
 function App() {
@@ -12,9 +17,8 @@ function App() {
     fetch("https://geolocation-db.com/json/0215bdd0-b516-11ec-b0a9-fdbfeccd28cf")
     .then(res => res.json())
     .then(data => {
-      console.log(data)
       setCenter([data.latitude, data.longitude]);
-      setLocation(data.city);
+      setLocation();
     })
   }
 
@@ -24,19 +28,33 @@ function App() {
   return (
     <div>
       <header>
-        <h1>City-Speak</h1>
+        <Grid container>
+          <Grid item xs={6}>
+            <h1>City-Speak</h1>
+          </Grid>
+          <Grid item xs={6}>
+            <div id="search-container">
+              <SearchIcon />
+              <input type="text" placeholder="Search" onChange={(e) => setLocation(e.target.value)} />
+            </div>
+          </Grid>
+        </Grid>
+        
       </header>
       <Grid container style={{minHeight: "80vh"}}>
         <Grid item xs={6}>
           <MyMap center={center}/>
         </Grid>
-        <Grid item xs={6}>
-          <h2>Current Location: {location}</h2>
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
+        <Grid item xs={6} className="right-side">
+          <Card className="card">
+            <h2>Current Location: {location}</h2>
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ul>
+          </Card>
+          
         </Grid>
       </Grid>
       
